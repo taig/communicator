19f2e27c23591625d98a6dfdae1dd5cf392efb1d
Taig <taig@AiR>
2013-07-12 07:45:52 +0200

    Added important fields to Response class.

--- src/main/java/com/taig/communicator/request/Response.java
+++ src/main/java/com/taig/communicator/request/Response.java
@@ -1,12 +1,39 @@
 package com.taig.communicator.request;
 
+import java.util.List;
+import java.util.Map;
+
 public class Response<T>
 {
+	protected int code;
+
+	protected String message;
+
+	protected Map<String, List<String>> headers;
+
 	protected T result;
 
-	public Response( T result )
+	public Response( int code, String message, Map<String, List<String>> headers, T result )
 	{
+		this.code = code;
+		this.message = message;
+		this.headers = headers;
 		this.result = result;
+	}
+
+	public int getCode()
+	{
+		return code;
+	}
+
+	public String getMessage()
+	{
+		return message;
+	}
+
+	public Map<String, List<String>> getHeaders()
+	{
+		return headers;
 	}
 
 	public T getPayload()
