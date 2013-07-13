0de199ec064786d23ab81fdff5b4c23af1b46d6e
Niklas Klein <my.taig@gmail.com>
2013-07-13 05:30:24 +0200

    Implemented Events sample.

--- sample/src/main/java/com/taig/communicator/sample/Main.java
+++ sample/src/main/java/com/taig/communicator/sample/Main.java
@@ -1,6 +1,7 @@
 package com.taig.communicator.sample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +30,7 @@ public class Main extends Activity
 			@Override
 			public void onClick( View view )
 			{
-				Toast.makeText( Main.this, "TODO", Toast.LENGTH_SHORT ).show();
+				startActivity( new Intent( Main.this, Events.class ) );
 			}
 		} );
 
