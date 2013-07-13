f266f249f58d23cb2a26bf8269fbaf222712f33c
Taig <taig@AiR>
2013-07-13 07:00:20 +0200

    Fixed gitignore that was hiding important sbt project definitions.

---
+++ project/Build.scala
@@ -0,0 +1,12 @@
+import sbt._
+import sbt.Keys._
+
+object Build extends sbt.Build
+{
+	val main = Project( "communicator", file( "." ) ).settings(
+		name := "Communicator",
+		organization := "com.taig.communicator",
+		version := "0.1",
+		autoScalaLibrary := false
+	)
+}
