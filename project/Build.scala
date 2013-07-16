dcb5b62121b3e6e927cca22605d8678268d97c67
Niklas Klein <my.taig@gmail.com>
2013-07-16 10:18:55 +0200

    Increased version number.

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object Build extends sbt.Build
 	val main = Project( "communicator", file( "." ) ).settings(
 		name := "Communicator",
 		organization := "com.taig.communicator",
-		version := "0.3.3",
+		version := "0.4",
 		autoScalaLibrary := false
 	)
 }
