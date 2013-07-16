50c5d42d5bd283d8d261c98b9c21bd8f8474a912
Niklas Klein <my.taig@gmail.com>
2013-07-16 10:15:37 +0200

    Rewrote Text Parser implementation because the current one did not forward occurring exceptions.

--- src/main/java/com/taig/communicator/result/Text.java
+++ src/main/java/com/taig/communicator/result/Text.java
@@ -1,16 +1,30 @@
 package com.taig.communicator.result;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Scanner;
 
 public class Text implements Parser<String>
 {
 	@Override
 	public String parse( URL url, InputStream stream ) throws IOException
 	{
-		Scanner scanner = new Scanner( stream, "UTF-8" ).useDelimiter( "\\A" );
-		return scanner.hasNext() ? scanner.next() : "";
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+
+		try
+		{
+			for( int length = 0; length != -1; length = stream.read( buffer ) )
+			{
+				bytes.write( buffer, 0, length );
+			}
+
+			return new String( bytes.toByteArray() );
+		}
+		finally
+		{
+			bytes.close();
+		}
 	}
 }
