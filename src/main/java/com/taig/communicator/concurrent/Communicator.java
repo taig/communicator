3ccc559e78f680554a04eca7108fee332e895096
Niklas Klein <my.taig@gmail.com>
2013-08-02 19:42:39 +0200

    Prepared basic communicator executor.

--- src/main/java/com/taig/communicator/concurrent/Communicator.java
+++ src/main/java/com/taig/communicator/concurrent/Communicator.java
@@ -1,44 +1,71 @@
 package com.taig.communicator.concurrent;
 
+import java.util.Deque;
 import java.util.List;
-import java.util.concurrent.AbstractExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Communicator extends AbstractExecutorService
 {
+	protected int connections;
+
+	protected Deque<Runnable> queue;
+
+	public Communicator( int connections )
+	{
+		if( connections < 1 )
+		{
+			throw new IllegalArgumentException( "At least 1 connection has to be granted" );
+		}
+
+		this.connections = connections;
+		this.queue = new LinkedBlockingDeque<Runnable>();
+	}
+
 	@Override
 	public void shutdown()
 	{
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public List<Runnable> shutdownNow()
 	{
-		throw new UnsupportedOperationException();
+		return null;
 	}
 
 	@Override
 	public boolean isShutdown()
 	{
-		throw new UnsupportedOperationException();
+		return false;
 	}
 
 	@Override
 	public boolean isTerminated()
 	{
-		throw new UnsupportedOperationException();
+		return false;
 	}
 
 	@Override
 	public boolean awaitTermination( long timeout, TimeUnit unit ) throws InterruptedException
 	{
-		throw new UnsupportedOperationException();
+		return false;
 	}
 
 	@Override
-	public void execute( Runnable command )
+	public void execute( Runnable runnable )
 	{
-		throw new UnsupportedOperationException();
+	}
+
+	protected class Thread extends java.lang.Thread
+	{
+		public Thread( Runnable runnable )
+		{
+			super( runnable );
+		}
+
+		@Override
+		public void run()
+		{
+			super.run();
+		}
 	}
 }
