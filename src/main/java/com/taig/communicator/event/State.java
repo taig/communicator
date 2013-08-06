212afda05f42ad62b515eb739db37f32a16a25d9
Niklas Klein <my.taig@gmail.com>
2013-08-06 20:22:05 +0200

    Added documentation to State.

--- src/main/java/com/taig/communicator/event/State.java
+++ src/main/java/com/taig/communicator/event/State.java
@@ -1,5 +1,8 @@
 package com.taig.communicator.event;
 
+/**
+ * All possible states that a {@link com.taig.communicator.request.Request} can have during its lifecycle.
+ */
 public enum State
 {
 	IDLE, START, CANCEL, SEND, RECEIVE, SUCCESS, FAILURE
