4ebde1ae722b0db94c8571acf775c4eae7c142e7
Niklas Klein <my.taig@gmail.com>
2013-08-06 20:04:57 +0200

    Defined static references for all available default parser.

--- src/main/java/com/taig/communicator/result/Parser.java
+++ src/main/java/com/taig/communicator/result/Parser.java
@@ -6,5 +6,11 @@ import java.net.URL;
 
 public interface Parser<T>
 {
+	public static final Ignore IGNORE = new Ignore();
+
+	public static final Image IMAGE = new Image();
+
+	public static final Text TEXT = new Text();
+
 	public T parse( URL url, InputStream stream ) throws IOException;
 }
