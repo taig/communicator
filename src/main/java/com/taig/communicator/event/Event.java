b2fef797ef9ec358feb4fcb75e70973be389f8a1
Niklas Klein <my.taig@gmail.com>
2013-08-07 02:27:03 +0200

    Added notice concerning main thread execution.

--- src/main/java/com/taig/communicator/event/Event.java
+++ src/main/java/com/taig/communicator/event/Event.java
@@ -13,8 +13,10 @@ import static com.taig.communicator.io.Updateable.*;
 
 /**
  * A collection of callback methods that shall be implemented by subclassing the Event class, overriding the desired
- * methods. Event callbacks are part of a {@link Request} and the overridden methods will
- * be executed during its lifecycle.
+ * methods. Event callbacks are part of a {@link Request} and the overridden methods will be executed during its
+ * lifecycle.
+ *
+ * All subclassed Event callbacks will be executed on the main thread!
  *
  * @param <R> The Request's {@link Response} type.
  */
