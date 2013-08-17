b23350a5bf8424ab69fa4ec3015ac12b014ffd73
Niklas Klein <my.taig@gmail.com>
2013-08-17 20:56:05 +0200

    Updated sample application to Android 4.3 dependencies.

--- sample/project/Build.scala
+++ sample/project/Build.scala
@@ -7,13 +7,12 @@ object Build extends sbt.Build
 {
 	val communicator = ProjectRef( file( "../" ), "communicator" )
 
-	val main = Project( "communicator-sample", file( "." ) )
-			.settings(
+	val main = Project( "communicator-sample", file( "." ) ).settings(
 				name := "CommunicatorSample",
 				version := "0.1",
 				versionCode := 0,
 				scalaVersion := "2.10.2",
-				platformName := "android-17",
+				platformName := "android-18",
 				libraryDependencies += "org.jsoup" % "jsoup" % "1.7.2" )
 			.dependsOn( communicator )
 			.aggregate( communicator )
