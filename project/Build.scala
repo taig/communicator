578b56a9ded1384028918fb225b8dc2efb6c0283
Niklas Klein <my.taig@gmail.com>
2013-08-17 03:40:38 +0200

    Updated android dependency to version 4.3.

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,6 @@ object Build extends sbt.Build
 		organization := "com.taig.communicator",
 		version := "0.6.2",
 		autoScalaLibrary := false,
-		libraryDependencies += "com.google.android" % "android" % "4.2" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-17/android.jar" )
+		libraryDependencies += "com.google.android" % "android" % "4.3" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-18/android.jar" )
 	)
 }
