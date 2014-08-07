0bc5a3fadd7c28be06f32b5c71f59cfd097709c4
Niklas Klein <my.taig@gmail.com>
2014-08-07 17:03:24 +0200

    Ordered settings alphabetically

--- project/Build.scala
+++ project/Build.scala
@@ -4,10 +4,10 @@ import sbt.Keys._
 object Build extends sbt.Build
 {
 	val main = Project( "communicator", file( "." ) ).settings(
+		autoScalaLibrary := false,
+		libraryDependencies += "com.google.android" % "android" % "4.4" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-19/android.jar" ),
 		name := "android-communicator",
 		organization := "com.taig",
-		version := "1.0.2",
-		autoScalaLibrary := false,
-		libraryDependencies += "com.google.android" % "android" % "4.4" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-19/android.jar" )
+		version := "1.0.2"
 	)
 }
