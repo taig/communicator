7958ef2cc6f6310bef5cb4216dd9172e2c8344e2
Niklas Klein <my.taig@gmail.com>
2014-08-07 16:41:08 +0200

    Changed project origanization and name

--- project/Build.scala
+++ project/Build.scala
@@ -4,8 +4,8 @@ import sbt.Keys._
 object Build extends sbt.Build
 {
 	val main = Project( "communicator", file( "." ) ).settings(
-		name := "Communicator",
-		organization := "com.taig.communicator",
+		name := "android-communicator",
+		organization := "com.taig",
 		version := "1.0.1",
 		autoScalaLibrary := false,
 		libraryDependencies += "com.google.android" % "android" % "4.3" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-18/android.jar" )
