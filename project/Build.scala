659fc24b2cbc8f2f83adc85e9aa60e9c08517864
Niklas Klein <my.taig@gmail.com>
2014-09-21 23:34:17 +0200

    Changed package name to com.taig.android

--- project/Build.scala
+++ project/Build.scala
@@ -7,8 +7,8 @@ object Build extends sbt.Build
 		autoScalaLibrary := false,
 		exportJars := true,
 		libraryDependencies += "com.google.android" % "android" % "4.4" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-19/android.jar" ),
-		name := "android-communicator",
-		organization := "com.taig",
+		name := "communicator",
+		organization := "com.taig.android",
 		scalaVersion := "2.11.2",
 		version := "1.0.3"
 	)
