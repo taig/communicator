831aaf21deaf5af6d2b0c57a4dc4c76f4793566a
Niklas Klein <my.taig@gmail.com>
2014-10-10 01:24:22 +0200

    Set project verion to 1.0.8
    Went back to basic sbt project to maintain an online repository / dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -10,6 +10,6 @@ object Build extends sbt.Build
 			name := "Communicator",
 			organization := "com.taig.android",
 			scalaVersion := "2.11.2",
-			version := "1.0.7"
+			version := "1.0.8"
 	)
 }
