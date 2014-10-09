5af2bc42f6496cae94f9d04772bef97a66e13200
Niklas Klein <my.taig@gmail.com>
2014-10-10 01:15:09 +0200

    Revert "Removed android.jar dependency, as we're now relying on pfn/android"
    This reverts commit a290a218f0d03f5fa7acb85a26a1396919e6efe6.
    
    Conflicts:
    	project/Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -7,6 +7,7 @@ object Build extends android.AutoBuild
 	lazy val main = Project( "communicator", file( "." ) )
 		.settings(
 			autoScalaLibrary := false,
+			libraryDependencies += "com.google.android" % "android" % "4.4" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-19/android.jar" ),
 			name := "Communicator",
 			organization := "com.taig.android",
 			scalaVersion := "2.11.2",
