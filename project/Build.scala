3bd65aae8a13b73fdc4ad670dcff3ef9aac30bc5
Niklas Klein <my.taig@gmail.com>
2014-10-09 15:54:11 +0200

    Revert "Integrated sbt-ghpages to build definition"
    This reverts commit d99906b31c7afdc87409c05b7766e8965c7ab979.

--- project/Build.scala
+++ project/Build.scala
@@ -1,20 +1,15 @@
-import android.Keys._
-import com.typesafe.sbt.SbtGhPages.ghpages
-import com.typesafe.sbt.SbtGit.git._
-import com.typesafe.sbt.SbtSite.site
-import sbt.Keys._
 import sbt._
+import sbt.Keys._
+import android.Keys._
 
 object Build extends android.AutoBuild
 {
 	lazy val main = Project( "communicator", file( "." ) )
-		.settings( site.settings ++ ghpages.settings: _* )
 		.settings(
 			autoScalaLibrary := false,
 			exportJars := true,
 			name := "Communicator",
 			organization := "com.taig.android",
-			remoteRepo := "https://github.com/Taig/Communicator.git",
 			scalaVersion := "2.11.2",
 			version := "1.0.7",
 			minSdkVersion in Android := "10",
