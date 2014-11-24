f6ad1c9ce1b42f77026cb62d547a71900539f4e8
Niklas Klein <my.taig@gmail.com>
2014-11-24 14:33:42 +0100

    Upgrade to Scala 2.11.4

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends sbt.Build
 			libraryDependencies += "com.google.android" % "android" % "4.4" % "provided" from ( "file://" + System.getenv( "ANDROID_HOME" ) + "/platforms/android-19/android.jar" ),
 			name := "Communicator",
 			organization := "com.taig.android",
-			scalaVersion := "2.11.2",
+			scalaVersion := "2.11.4",
 			version := "1.0.7"
 	)
 }
