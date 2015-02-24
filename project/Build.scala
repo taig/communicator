31614a4b372339d26620fb5bcafdf60c699722ec
Niklas Klein <my.taig@gmail.com>
2015-02-24 20:00:48 +0100

    Add publish configuration

--- project/Build.scala
+++ project/Build.scala
@@ -6,14 +6,54 @@ extends	sbt.Build
 {
 	lazy val main = Project( "communicator", file( "." ) )
 		.settings(
+			description := "An OkHttp wrapper for Scala built with Android in mind",
 			libraryDependencies ++= Seq(
 				"com.squareup.okhttp" % "okhttp" % "2.2.0",
 				"org.scalatest" %% "scalatest" % "2.2.4" % "test",
 				"org.mock-server" % "mockserver-netty" % "3.9.1" % "test"
 			),
+			licenses := Seq( "MIT" -> url( "https://raw.githubusercontent.com/taig/communicator/master/LICENSE" ) ),
+			homepage := Some( url( "https://github.com/taig/communicator" ) ),
+			name := "Communicator",
 			organization := "io.taig",
+			organizationHomepage := Some( url( "http://taig.io" ) ),
+			pomExtra := pom,
+			pomIncludeRepository := { _ => false },
+			publishArtifact in Test := false,
+			publishMavenStyle := true,
+			publishTo <<= version ( version =>
+			{
+				val repository = if( version.endsWith( "SNAPSHOT" ) )
+				{
+					( "snapshot", "/content/repositories/snapshots" )
+				}
+				else
+				{
+					( "release", "/service/local/staging/deploy/maven2" )
+				}
+
+				Some( repository._1 at "https://oss.sonatype.org" + repository._2 )
+			} ),
 			scalacOptions ++= Seq( "-deprecation", "-feature" ),
 			scalaVersion := "2.11.5",
+			scmInfo := Some(
+				ScmInfo(
+					url( "https://github.com/taig/communicator" ),
+					"git@github.com:taig/communicator.git"
+				)
+			),
+			startYear := Some( 2013 ),
 			version := "2.0.0"
 		)
+
+	val pom =
+	{
+		<developers>
+			<developer>
+				<id>Taig</id>
+				<name>Niklas Klein</name>
+				<url>http://taig.io/</url>
+			</developer>
+		</developers>
+	}
 }
