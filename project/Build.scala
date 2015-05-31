fc36c428dfd3f3d34c2ceb84ce589ff4a02c6252
Niklas Klein <mail@taig.io>
2015-05-31 14:49:22 +0200

    Update version string

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,7 @@ extends	sbt.Build
 			organization := "io.taig",
 			scalacOptions ++= Seq( "-deprecation", "-feature" ),
 			scalaVersion := "2.11.6",
-			version := "2.0.2-SNAPSHOT"
+			version := "2.0.2"
 		)
 		.settings(
 			description := "An OkHttp wrapper for Scala built with Android in mind",
