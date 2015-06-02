4ab50c1b2b9a105c62acdc7ccb2c100bbef7efd4
Niklas Klein <mail@taig.io>
2015-06-02 14:22:30 +0200

    Prepare next development iteration

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,7 @@ extends	sbt.Build
 			organization := "io.taig",
 			scalacOptions ++= Seq( "-deprecation", "-feature" ),
 			scalaVersion := "2.11.6",
-			version := "2.0.2"
+			version := "2.1.0-SNAPSHOT"
 		)
 		.settings(
 			description := "An OkHttp wrapper for Scala built with Android in mind",
