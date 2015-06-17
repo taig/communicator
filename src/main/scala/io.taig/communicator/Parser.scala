1a927bfd02c214304a3229e017c354246cd336e6
Niklas Klein <mail@taig.io>
2015-06-17 22:19:46 +0200

    Remove string iterator parser

--- src/main/scala/io.taig/communicator/Parser.scala
+++ src/main/scala/io.taig/communicator/Parser.scala
@@ -19,27 +19,17 @@ object Parser
 		override def parse( response: Response, stream: InputStream ) = null.asInstanceOf[Nothing]
 	}
 
-	implicit val iteratorString = new Parser[Iterator[String]]
+	implicit val string = new Parser[String]
 	{
-		val utf8 = Charset.forName( "UTF-8" )
-
 		override def parse( response: Response, stream: InputStream ) =
 		{
 			val charset = Option( response.headers.get( "Content-Type" ) )
 				.map( MediaType.parse )
 				.map( _.charset() )
 				.flatMap( Option.apply )
-				.getOrElse( utf8 )
+				.getOrElse( Charset.forName( "UTF-8" ) )
 
-			Source.fromInputStream( stream, charset.displayName() ).getLines()
-		}
-	}
-
-	implicit val string = new Parser[String]
-	{
-		override def parse( response: Response, stream: InputStream ) =
-		{
-			iteratorString.parse( response, stream ).mkString
+			Source.fromInputStream( stream, charset.displayName() ).mkString
 		}
 	}
 }
