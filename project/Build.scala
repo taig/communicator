de652fef9ea5d40f9da03b660571307dc6b9667e
Niklas Klein <mail@taig.io>
2015-08-26 13:24:41 +0200

    Extract main build settings

--- project/Build.scala
+++ project/Build.scala
@@ -8,29 +8,6 @@ extends	sbt.Build
 {
 	val main = Project( "communicator", file( "." ), settings = sonatypeSettings )
 		.settings(
-			javacOptions ++= (
-				"-source" :: "1.7" ::
-				"-target" :: "1.7" ::
-				Nil
-			),
-			libraryDependencies ++= (
-				"com.squareup.okhttp" % "okhttp" % "2.5.0" ::
-				"ch.qos.logback" % "logback-classic" % "1.1.3" % "test" ::
-				"org.scalatest" %% "scalatest" % "2.2.5" % "test" ::
-				"org.mock-server" % "mockserver-netty" % "3.9.17" % "test" ::
-				Nil
-			),
-			name := "Communicator",
-			organization := "io.taig",
-			scalacOptions ++= (
-				"-deprecation" ::
-				"-feature" ::
-				Nil
-			),
-			scalaVersion := "2.11.7",
-			version := "2.2.1-SNAPSHOT"
-		)
-		.settings(
 			description := "An OkHttp wrapper for Scala built with Android in mind",
 			homepage := Some( url( "https://github.com/taig/communicator" ) ),
 			licenses := Seq( "MIT" -> url( "https://raw.githubusercontent.com/taig/communicator/master/LICENSE" ) ),
