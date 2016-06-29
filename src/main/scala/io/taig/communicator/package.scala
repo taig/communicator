4c150a6d7edeebf476bf15087ad0fd9a2c9ba24e
Niklas Klein <mail@taig.io>
2016-06-29 14:53:35 +0200

    Refactor Client to live in package object

--- src/main/scala/io/taig/communicator/package.scala
+++ src/main/scala/io/taig/communicator/package.scala
@@ -1,5 +1,17 @@
 package io.taig
 
+import okhttp3.OkHttpClient
+
 package object communicator {
-    type Client = okhttp3.OkHttpClient
+    type Client = OkHttpClient
+
+    object Client {
+        type Builder = OkHttpClient.Builder
+
+        object Builder {
+            def apply() = new Builder()
+        }
+
+        def apply() = new Client
+    }
 }
