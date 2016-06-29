a962431a3ddf6f7513fbee42ab0a9112091bf91a
Niklas Klein <mail@taig.io>
2016-06-29 14:44:23 +0200

    Add Client.Builder.apply()

--- src/main/scala/io/taig/communicator/Client.scala
+++ src/main/scala/io/taig/communicator/Client.scala
@@ -5,5 +5,9 @@ import okhttp3.OkHttpClient
 object Client {
     type Builder = OkHttpClient.Builder
 
+    object Builder {
+        def apply() = new Builder()
+    }
+
     def apply() = new Client
 }
--- src/main/scala/io/taig/communicator/package.scala
+++ src/main/scala/io/taig/communicator/package.scala
@@ -1,7 +1,5 @@
 package io.taig
 
-import monix.eval.Task
-
 package object communicator {
     type Client = okhttp3.OkHttpClient
 }
