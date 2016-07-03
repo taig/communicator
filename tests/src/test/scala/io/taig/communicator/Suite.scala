4fc58b0842244245fa9f10921e79c850ee3289ba
Niklas Klein <mail@taig.io>
2016-07-03 11:44:25 +0200

    Let's just try a bigger thread pool again

--- tests/src/test/scala/io/taig/communicator/Suite.scala
+++ tests/src/test/scala/io/taig/communicator/Suite.scala
@@ -12,7 +12,7 @@ trait Suite
         with Matchers {
     LogManager.getLogManager.reset()
 
-    implicit val scheduler = Scheduler.fixedPool( "tests", 1 )
+    implicit val scheduler = Scheduler.fixedPool( "tests", 3 )
 
     implicit def client = Client()
 
