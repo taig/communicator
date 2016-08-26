85b5cbf6d7c1d68c5f8bce7b172bc3cb6e1ebe92
Niklas Klein <mail@taig.io>
2016-08-26 10:49:24 +0200

    Add codecov token

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ set -e # halt on errors
 
 cd ~/phoenix_echo/ && elixir --detached -S mix do phoenix.server
 cd /communicator/ && sbt ";coverage;test;coverageReport;coverageAggregate;tut"
-codecov
+codecov -t $CODECOV_TOKEN
