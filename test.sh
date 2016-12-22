35fa0ae13c82c00e5654ee5c972802a19db5fc74
Niklas Klein <mail@taig.io>
2016-12-22 13:52:46 +0100

    Enable cross version testing on CI

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ cd ~/phoenix_echo/
 elixir --detached -S mix do phoenix.server
 cd -
 
-sbt ";coverage;test;tut;coverageReport;coverageAggregate"
+sbt ";coverage;+test;+tut;coverageReport;coverageAggregate"
 
 if [ -n "$CODECOV_TOKEN" ]; then
     codecov
