7add51b5b2d8a20cba7a35f296e4607f0bed6f48
Niklas Klein <mail@taig.io>
2018-04-25 10:44:04 +0200

    Remove unused implicit conversion

--- request/src/main/scala/io/taig/communicator/package.scala
+++ request/src/main/scala/io/taig/communicator/package.scala
@@ -1,12 +1,6 @@
 package io.taig
 
-import scala.language.implicitConversions
-
 package object communicator {
-  implicit private[communicator] def function0ToRunnable(
-      f: () ⇒ Unit
-  ): Runnable = new Runnable { override def run() = f() }
-
   type OkHttpRequest = okhttp3.Request
 
   object OkHttpRequest {
